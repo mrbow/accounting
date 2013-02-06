207c2bfddc9cfc2b7542e07a0e71f2e2c5e89562
mrbow@tzi.de <mrbow@tzi.de>
2013-02-06 12:22:32 +0100

    create ruby structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accounting::Application.config.secret_token = 'd453bdbc735e509bfb90e86107f8a57f53b55f21d6dedb3222bdb72896c9f0c7698b757e5ccf3bffa03d65faa2817dfe21beae56f63a291e3ca3da14080c6b08'
